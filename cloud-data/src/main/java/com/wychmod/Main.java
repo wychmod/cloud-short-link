cb2224ccbcbe08817455dea8982bbbb54dfd35ba
wychmod <545480453@qq.com>
2025-06-24 00:42:01 +0800

    feat: 初始化云原生项目- 添加 .gitignore 文件，忽略构建工具和 IDE 的配置文件- 创建多个子模块：cloud-account、cloud-app、cloud-common、cloud-data、cloud-gateway、cloud-link、cloud-shop - 每个子模块包含一个简单的 Main 类，打印 "Hello world!" - 创建根目录下的 pom.xml 文件，定义项目结构和公共属性

---
+++ cloud-data/src/main/java/com/wychmod/Main.java
@@ -0,0 +1,12 @@
+package com.wychmod;
+
+/**
+ * @description:
+ * @author: wychmod
+ * @date: 2025-06-24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
