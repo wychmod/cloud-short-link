dcfd2a85e3fdbc49cf4f38584751246b595758dc
wychmod <545480453@qq.com>
2025-07-20 22:38:11 +0800

    feat(account): 添加验证码功能
    - 在 NotifyController 中添加 getCaptcha 方法，生成并返回验证码图片
    - 使用 Redis 存储验证码文本，设置过期时间
    - 新增 getCaptchaKey 方法生成验证码的唯一键
    - 在 RedisTemplateConfiguration 中配置 StringRedisTemplate

--- cloud-account/src/main/java/com/wychmod/controller/NotifyController.java
+++ cloud-account/src/main/java/com/wychmod/controller/NotifyController.java
@@ -1,7 +1,11 @@
 package com.wychmod.controller;
 
 import com.google.code.kaptcha.Producer;
+import com.wychmod.utils.CommonUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @description:
@@ -27,20 +32,37 @@ public class NotifyController {
     @Resource
     public Producer captchaProducer;
 
+    @Autowired
+    public StringRedisTemplate redisTemplate;
+
+    /**
+     * 验证码过期时间
+     */
+    private static final long CAPTCHA_CODE_EXPIRED = 1000 * 10 *  60;
+
     @GetMapping("/captcha")
     public void getCaptcha(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String captchaProducerText = captchaProducer.createText();
         log.info("验证码：{}", captchaProducerText);
+
+        redisTemplate.opsForValue().set(getCaptchaKey(request),captchaProducerText,CAPTCHA_CODE_EXPIRED, TimeUnit.MILLISECONDS);
+
         BufferedImage bufferedImage = captchaProducer.createImage(captchaProducerText);
 
-        try {
-            ServletOutputStream outputStream = response.getOutputStream();
+        try (ServletOutputStream outputStream = response.getOutputStream()){
             ImageIO.write(bufferedImage,"jpg",outputStream);
             outputStream.flush();
-            outputStream.close();
         } catch (IOException e) {
             log.error("获取流出错:{}",e.getMessage());
         }
+    }
+
+    private String getCaptchaKey(HttpServletRequest request) {
+        String ip = CommonUtil.getIpAddr(request);
+        String userAgent = request.getHeader("User-Agent");
+        String key = "account-service:captcha:"+CommonUtil.MD5(ip+userAgent);
+        log.info("验证码key:{}",key);
+        return key;
     }
 
 
